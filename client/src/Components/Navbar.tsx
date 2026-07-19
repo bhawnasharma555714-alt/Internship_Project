5df55b011a756679a1db8ad25a6e7f7c0e4554f2
bhawnasharma555714-alt <bhawnasharma555714@gmail.com>
2026-07-19 15:51:54 +0530

    Home and Navbar styling

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,26 +1,28 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
+import { Orbit } from "lucide-react";
 
 function Navbar(){
     const {user,logout} = useAuth();
     return(
-        <nav className="bg-white shadow-md sticky top-0 z-50">
+        <nav className="bg-slate-900 shadow-md sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-6">
-                {/* <Logo/> */}
-                <div className="flex items-center justify-between h-16">
-                    <Link to="/" className="text-2xl font-bold text-violet-600">Collab Connect</Link>
-                    <div className="flex items-center gap-6">
-                        <Link to='/' className="text-gray-700 hover:text-violet-600 transition-colors">Home</Link>
-                        <Link to='/projects' className="text-gray-700 hover:text-violet-600 transition-colors">Projects</Link>
+                <div className="flex items-center justify-between h-20">
+                    <Link to="/" className="flex items-center gap-2 text-sky-600 text-3xl font-extrabold"><Orbit size={32} strokeWidth={2.5} />
+                            <span className="text-2xl font-bold">CollabConnect</span>
+                    </Link>
+                    <div className="flex items-center gap-8">
+                        <Link to='/' className="text-slate-400 hover:text-sky-500 hover:-translate-y-1 duration-100 transition-colors">Home</Link>
+                        <Link to='/projects' className="text-slate-400 hover:text-sky-500 hover:-translate-y-1 duration-100 transition-colors">Projects</Link>
                         {user? (
                             <>
-                                <Link to='/profile' className="text-gray-700 hover:text-violet-600 transition-colors">Profile</Link>
-                                <Link to='/my-projects' className="text-gray-700 hover:text-violet-600 transition-colors">My Projects</Link>
-                                <Link to='/my-applications' className="text-gray-700 hover:text-violet-600 transition-colors">My Applications</Link>
-                                <button className="bg-violet-600 text-white px-4 py-2 rounded-lg hover:bg-violet-700 transition-colors" onClick={logout}>Logout</button>
+                                <Link to='/profile' className="text-slate-400 hover:text-sky-500 hover:-translate-y-1 duration-100 transition-colors">Profile</Link>
+                                <Link to='/my-projects' className="text-slate-400 hover:text-sky-500 hover:-translate-y-1 duration-100 transition-colors">My Projects</Link>
+                                <Link to='/my-applications' className="text-slate-400 hover:text-sky-500 hover:-translate-y-1 duration-100 transition-colors">My Applications</Link>
+                                <button className="bg-sky-700 text-white px-4 py-2 rounded-lg hover:bg-sky-600 transition-colors" onClick={logout}>Logout</button>
                             </>
                         ) : (
-                            <Link to='/login'>Login</Link>
+                            <Link to='/login' className="bg-sky-700 text-white px-4 py-2 rounded-lg hover:bg-sky-600 transition-colors">Login</Link>
                         )}
                     </div>
                 </div>
