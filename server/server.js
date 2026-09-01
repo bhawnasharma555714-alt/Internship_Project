52bedc9f376535155218c177e2f38ea94e5510a8
bhawnasharma555714-alt <bhawnasharma555714@gmail.com>
2026-09-01 21:09:15 +0530

    Fix

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://collabconnect1.vercel.app"],
         methods: ["GET", "POST"]
     }
 });
