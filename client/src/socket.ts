04d91e482accae28cff7b0205508e90d7ed13ba9
bhawnasharma555714-alt <bhawnasharma555714@gmail.com>
2026-09-01 21:01:09 +0530

    Fix

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
-const socket = io("http://localhost:3000",{
+const socket = io("https://internship-project-backend-8lwm.onrender.com",{
     auth: {
         token: localStorage.getItem("token")
     }
