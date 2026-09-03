2e1d83f5234018510224e27b115648a86477735c
bhawnasharma555714-alt <bhawnasharma555714@gmail.com>
2026-09-03 13:55:38 +0530

    Socket fix

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,6 +2,7 @@ import {io} from 'socket.io-client'
 const socket = io("https://internship-project-backend-8lwm.onrender.com",{
     auth: {
         token: localStorage.getItem("token")
-    }
+    },
+    transports: ["polling", "websocket"], 
 });
 export default socket;
